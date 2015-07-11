a70f78e5a0fea570522361db50a27956e8c32196
miciek <miciek@gmail.com>
2015-07-11 23:46:49 +0200

    hostname, password and username are now taken from .netrc file (curlftpfs)

--- Apps/tools/klaus_mount.sh
+++ Apps/tools/klaus_mount.sh
@@ -1,4 +1,10 @@
 #!/bin/bash
 
-sudo curlftpfs -o allow_other micio@10.10.10.1 /rmt/klaus
+sudo curlftpfs -o allow_other ftp://10.10.10.1 /rmt/klaus
+
+# It uses .netrc file in home directory,
+# The format is:
+#    machine ftp.host.com  
+#    login myuser  
+#    password mypass 
 
