7f0bc5d374c0ec7d0c4afb96742769d355a3216d
Michał Płachta <miciek@exul.net>
2024-12-04 09:09:03 +0100

    Update metals config

--- nvim/lua/plugins/metals.lua
+++ nvim/lua/plugins/metals.lua
@@ -16,9 +16,7 @@ return {
     local nvim_metals_group = vim.api.nvim_create_augroup("nvim-metals", { clear = true })
     metals_config.settings = {
       showImplicitArguments = true,
-      showInferredType = false,
-      superMethodLensesAvailable = true,
-      showImplicitConversionAndClasses = false
+      showInferredType = false
     }
     metals_config.find_root_dir_max_project_nesting = 2
     vim.api.nvim_create_autocmd("FileType", {
