53800b6c50dde352cc33916c905375b4f00edacc
Michał Płachta <miciek@exul.net>
2025-01-09 09:50:07 +0100

    Remove no-neck-pain, add file-browser and configure neotree

---
+++ nvim/lua/plugins/neotree.lua
@@ -0,0 +1,17 @@
+-- add custom neo-tree options
+-- see https://github.com/nvim-neo-tree/neo-tree.nvim/blob/main/lua/neo-tree/defaults.lua
+return {
+  "nvim-neo-tree/neo-tree.nvim",
+  opts = function(_, opts)
+    opts.window.width = 80
+    opts.filesystem = {
+      filtered_items = {
+        visible = true,
+        hide_gitignored = false,
+        hide_dotfiles = false,
+        hide_by_name = {},
+        never_show = { ".git" },
+      },
+    }
+  end,
+}
