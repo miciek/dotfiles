41ec41788f0141391b49ff0593ff97e61d4c6c38
Michał Płachta <miciek@exul.net>
2025-04-05 10:55:56 +0200

    Make sure ASToggle works on startup

--- nvim/init.lua
+++ nvim/init.lua
@@ -1,2 +1,9 @@
 -- bootstrap lazy.nvim, LazyVim and your plugins
 require("config.lazy")
+
+vim.api.nvim_create_autocmd("VimEnter", {
+  callback = function()
+    vim.cmd("ASToggle")
+    vim.cmd("ASToggle")
+  end,
+})
